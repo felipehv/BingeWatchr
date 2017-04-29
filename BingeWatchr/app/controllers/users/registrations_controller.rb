a05149e3b62cdd44f6d1a291807fc5aa6640602d
Felipe Haase <fahaase@uc.cl>
2017-04-29 20:20:26 -0300

    AddSons

--- BingeWatchr/app/controllers/users/registrations_controller.rb
+++ BingeWatchr/app/controllers/users/registrations_controller.rb
@@ -3,45 +3,55 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+   def new
+     #@user_id = current_user.id
+    super
+   end
+
+  def new0
+    @users = User.new
+    @pid = current_user.id
+  end
+
+  def create_son
+    User.create(email: params[:email], password: params[:password], parent_id: params[:parent_id])
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
   # cancel oauth signing in/up in the middle of the process,
   # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  def cancel
+    super
+  end
 
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :admin, :parent_id])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
@@ -49,12 +59,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+   super(resource)
+  end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    super(resource)
+  end
 end
--- BingeWatchr/config/routes.rb
+++ BingeWatchr/config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   resources :capitulos
   resources :series
   mount Ckeditor::Engine => '/ckeditor'
-  devise_for :users# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: 'users/registrations'  }# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "posts#index"
+  devise_scope :user do get '/users/newson' => 'users/registrations#new0' end
+  devise_scope :user do post '/users/createson' => 'users/registrations#create_son' end
 
   resources :posts
 end
