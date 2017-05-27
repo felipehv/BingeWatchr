25fbc7f7f8de82132e805e0e8a764c6831f40cc4
Felipe Haase <fahaase@uc.cl>
2017-05-27 16:17:09 -0400

    Rutas y vistas para CRUD de usuarios con diferenciacion entre normales y admins

--- BingeWatchr/config/routes.rb
+++ BingeWatchr/config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
   devise_scope :user do get '/profile' => "users/registrations#profile" end
   devise_scope :user do get '/users/newson' => 'users/registrations#new0' end
   devise_scope :user do post '/users/createson' => 'users/registrations#create_son' end
-  devise_scope :user do get '/users/edit_son/:id' => 'users/registrations#edit0' end
-  devise_scope :user do post '/users/edit_son' => 'users/registrations#edit_son' end
+  devise_scope :user do get '/users/edit_user/:id' => 'users/registrations#edit0' end
   devise_scope :user do post '/users/create_son' => 'users/registrations#create_son' end
+  devise_scope :user do post '/users/delete_user' => 'users/registrations#delete_user' end
+  devise_scope :user do post '/users/edit_user' => 'users/registrations#edit_user' end
+  devise_scope :user do post '/users/create_admin' => 'users/registrations#create_admin' end
 
 
   resources :posts
