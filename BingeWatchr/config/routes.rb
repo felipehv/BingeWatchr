a210ae346a2b64d9739483221fb97213e1c263a3
Felipe Andrés Haase Vargas <fahaase@uc.cl>
2017-05-28 19:53:24 -0400

    Update routes.rb

--- BingeWatchr/config/routes.rb
+++ BingeWatchr/config/routes.rb
@@ -4,10 +4,17 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users, controllers: { registrations: 'users/registrations'  }# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "posts#index"
-  devise_scope :user do get '/users/newson' => 'users/registrations#new0' end
+  devise_scope :user do get '/profile' => "main#profile" end
+  devise_scope :user do get '/users/newson' => 'main#new0' end
   devise_scope :user do post '/users/createson' => 'users/registrations#create_son' end
+  devise_scope :user do get '/users/edit_user/:id' => 'main#edit0' end
+  devise_scope :user do post '/users/create_son' => 'users/registrations#create_son' end
+  devise_scope :user do post '/users/delete_user' => 'users/registrations#delete_user' end
+  devise_scope :user do post '/users/edit_user' => 'users/registrations#edit_user' end
+  devise_scope :user do post '/users/create_admin' => 'users/registrations#create_admin' end
 
-  get 'series' => 'seires#index'
+
+  get 'series' => 'series#index'
 
   get 'series/:id' => 'series#show', as: :tag
 
@@ -16,4 +23,6 @@ Rails.application.routes.draw do
   put 'capitulos/:id', to: 'capitulos#update'
 
   resources :posts
+  resources :users
 end
+
