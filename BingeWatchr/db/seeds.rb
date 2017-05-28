bd5e790b1fb293e09f186707e0f5f61ed2a1d12c
Tomás Suárez <tsuarezsaieh@gmail.com>
2017-05-27 21:12:27 -0400

    heroku

--- BingeWatchr/db/seeds.rb
+++ BingeWatchr/db/seeds.rb
@@ -36,21 +36,13 @@ Post.destroy_all
 posts_list.each do |title, body, created, updated|
   Post.create( title: title, body: body, created_at: created, updated_at: updated)
 end
+
 Tipo.destroy_all
 tipos_list.each do |tipo, created, updated|
   Tipo.create(name: tipo, created_at: created, updated_at: updated)
-<<<<<<< HEAD
-<<<<<<< Updated upstream
 end
-=======
-end
+
 
 User.destroy_all
 User.create(email: "admin@uc.cl", password: "123456", admin: 1)
->>>>>>> Stashed changes
-=======
-end
 
-
-User.create(email: "admin@uc.cl", password: "123456", admin: 1)
->>>>>>> 8d3e3bf4e25cf3bfc45ef1c2903c34744cbe8c24
