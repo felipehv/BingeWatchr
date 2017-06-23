fb03f5823f8475266cb261275a541171c081a384
Tomás Suárez <tsuarezsaieh@gmail.com>
2017-06-23 17:52:05 -0400

    buscador

--- BingeWatchr/app/models/serie.rb
+++ BingeWatchr/app/models/serie.rb
@@ -2,4 +2,11 @@ class Serie < ApplicationRecord
   belongs_to :user
   belongs_to :tipo
   has_many :capitulos
+  def self.search(search)
+	  if search
+	    where('lower(name) LIKE ?', "%#{search}%".downcase)
+	  else
+	    all
+	  end
+	end
 end
