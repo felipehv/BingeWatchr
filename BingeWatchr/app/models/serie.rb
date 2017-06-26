cbafc67c4ef18639486b0778be35a330187a6d6e
Tomás Suárez <tsuarezsaieh@gmail.com>
2017-06-25 21:34:47 -0400

    restriccion de quienes ven las series

--- BingeWatchr/app/models/serie.rb
+++ BingeWatchr/app/models/serie.rb
@@ -1,7 +1,7 @@
 class Serie < ApplicationRecord
     has_many :capitulos
-    def self.filter(search, tipo_id)
-      results = self.all
+    def self.filter(search, tipo_id, series)
+      results = series
       results = results.where('lower(name) LIKE ?', "%#{search}%".downcase) if search.present?
       results = results.where("series.tipo_id = ?", tipo_id) if tipo_id.present?
       results
